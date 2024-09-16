def0c5b27455fe924b6514402c6c4e9ca24019d7
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-16 17:00:44 +0600

    Completed Scroll bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
 import './Header.css';
 import logo from '../../assets/Logo/Valuetainment.svg'
 import { IoMenu } from "react-icons/io5";
+import img_png_9 from '../../assets/img/img (9).png'
+import img_png_11 from '../../assets/img/img (11).png'
+import img_png_10 from '../../assets/img/img (10).png'
+import img_jpeg_11 from '../../assets/img/img (11).jpeg'
 
 const Header = () => {
     return (
@@ -12,12 +16,99 @@ const Header = () => {
                 </div>
                 <div className='nav-menu'>
                     <IoMenu className='text-white font-bold w-full h-full'></IoMenu>
+                    <ul className='nav-profile-dropdown'>
+                            <li className='cursor-pointer hover:text-orange-500'>Home</li> 
+                            <li className='cursor-pointer hover:text-orange-500'>About Us</li>
+                            <li className='cursor-pointer hover:text-orange-500'>Contact Us</li>
+                        </ul>
                 </div>
             </nav>
             {/* Banner */}
-            <div className='banner-div'>
-                <div className='banner-text '>
+            <div className='banner-div h-full relative'>
+                <div className='banner-text'>
                    future looks brights!
+                </div>
+                <div className='banner-adds absolute overflow-x-auto  bottom-0 left-0 right-0 top-20'>
+                    <div className="banner-adds-div">
+                        <img src={img_png_9} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_png_10} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_jpeg_11} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_png_11} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_png_9} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_png_10} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_jpeg_11} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="banner-adds-div">
+                        <img src={img_png_11} alt="" />
+                        <div>
+                            <p className='dress-name'>Army Green Active Short Sleeve</p>
+                            <div className='flex gap-3'>
+                                <p className='dress-price'>$ 34.95</p>
+                                <p className='line-through dress-price'>$ 34.95</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
