597b74cac4e2353ac471d04f8fd13671ce7b5d88
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-17 00:04:46 +0600

    Mission Part completed

--- src/components/ShopNow/ShopNow.jsx
+++ src/components/ShopNow/ShopNow.jsx
@@ -4,10 +4,13 @@ import { GoArrowRight } from "react-icons/go";
 import pat_signature from "../../assets/img/pat-signature White 1.png"
 import img_png_08 from "../../assets/img/img (8).png"
 import img_png_01 from "../../assets/img/img (1).png"
+import img_png_05 from "../../assets/img/img (5).png"
+import icon from "../../assets/Icon/quotes.svg"
 
 const ShopNow = () => {
     return (
-        <div className='shop'>
+       <div className='main relative'>
+         <div className='shop'>
             <div className="shop-left">
                 <p className='shop-left-title'>PATRICK BET-DAVID'S </p>
                 <p className='shop-left-signed'>SINGED <br></br> COLLECTION</p>
@@ -17,13 +20,27 @@ const ShopNow = () => {
                 </div>
                 <img src={pat_signature} alt="" className='pat-sign' />
             </div>
-            <div className="shop-right flex">
+            <div className="shop-right flex items-center">
                 <img src={img_png_01} alt="" className='shop-right-img' />
-                <img src={img_png_08} alt="" className='shop-right-img' />
+                <img src={img_png_08} alt="" className='shop-right-img-02' />
 
             </div>
 
         </div>
+        <div className='bottom flex'>
+            <div className='relative bottom-left'>
+                <img src={icon} alt="" className='icon-style '/>
+                <div className='bottom-text-left'>
+                    <p className='bottom-text'>OUR MISSION IS TO ENLIGHTEN ENTERTAIN</p>
+                    <p className='bottom-text'>AND EMPOWER CURRENT AND FUTURE</p>
+                    <p className='bottom-text'>LEADER AROUND THE WORLD</p>
+                </div>
+            </div>
+            <div className="bottom-right">
+                    <img src={img_png_05} alt="" className='bottom-right-img'/>
+            </div>
+        </div>
+       </div>
     );
 };
 
