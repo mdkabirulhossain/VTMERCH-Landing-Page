1bf3f4f68f082136bd1896f931008d4943c8c3c8
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-09-16 22:02:38 +0600

    Best Selling product part is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import BestSellProduct from './components/BestSellProduct/BestSellProduct'
 import Header from './components/Header/Header'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <BestSellProduct></BestSellProduct>
     </>
   )
 }
---
+++ src/components/ShopNow/ShopNow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ShopNow.css';
+
+const ShopNow = () => {
+    return (
+        <div className='shop'>
+            
+        </div>
+    );
+};
+
+export default ShopNow;
